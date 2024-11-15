507ca1ab5ca1d184c4874649509688fc9e4fb9ee
Mikteor <mictor@bykollab.com>
2024-11-15 19:37:10 +0100

    bot handler

---
+++ src/app/api/telegram-webhook/route.ts
@@ -0,0 +1,15 @@
+// pages/api/telegram-webhook.ts
+import { Markup, Telegraf } from 'telegraf';
+
+// Инициализация бота с использованием токена из переменных окружения
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN || '');
+const WEB_APP_URL = "t.me/Miktool_bot/Miktool";
+
+bot.command("inlinekb", ctx =>
+	ctx.reply(
+		"Hello! You can open the Miktool app by clicking the button below.",
+		Markup.inlineKeyboard([Markup.button.webApp("Launch", WEB_APP_URL)]),
+	),
+);
+
+bot.launch();
