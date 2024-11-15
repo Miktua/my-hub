4e420c474055c6cabb108f88bc788503ef948d2f
Mikteor <mictor@bykollab.com>
2024-11-16 00:57:22 +0100

    fix

--- src/app/api/telegram-webhook/route.ts
+++ src/app/api/telegram-webhook/route.ts
@@ -1,9 +1,14 @@
 // pages/api/telegram-webhook.ts
+import { NextApiResponse } from 'next';
+import { NextApiRequest } from 'next';
+import { NextResponse } from 'next/server';
 import { Markup, Telegraf } from 'telegraf';
 
 // Инициализация бота с использованием токена из переменных окружения
 const bot = new Telegraf(process.env.NEXT_TELEGRAM_BOT_TOKEN || '');
 const WEB_APP_URL = "t.me/Miktool_bot/Miktool";
+
+bot.on("text", ctx => ctx.reply("Hello"));
 
 bot.command("inlinekb", ctx =>
 	ctx.reply(
@@ -13,3 +18,14 @@ bot.command("inlinekb", ctx =>
 );
 
 bot.launch();
+
+export default async function POST(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        await bot.handleUpdate(req.body, res);
+        return NextResponse.json({ message:'Updated' }, { status: 200 })
+    } catch (error) {
+        console.error('Error handling update:', error);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
+    }
+
+}
