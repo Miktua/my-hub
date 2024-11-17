9f54a8697eca572893a4b4bea922fa2c408f0c50
Mikteor <mictor@bykollab.com>
2024-11-17 18:16:00 +0100

    url fix attempt

--- src/app/api/telegram-webhook/route.ts
+++ src/app/api/telegram-webhook/route.ts
@@ -8,7 +8,6 @@ import { Bot, InlineKeyboard, webhookCallback } from 'grammy'
 const TOKEN = process.env.NEXT_TELEGRAM_BOT_TOKEN || ''
 const WEB_APP_URL = process.env.NEXT_MIKTOOL_APP_URL || '';
 
-
 if (!TOKEN) throw new Error('TELEGRAM_BOT_TOKEN environment variable not found.')
 
 
@@ -17,8 +16,8 @@ const bot = new Bot(TOKEN)
 // bot.on('message:text', async (ctx) => {
 //   await ctx.reply(ctx.message.text)
 // })
-
-const inlineKeyboard = new InlineKeyboard().webApp("Launch", WEB_APP_URL);
+console.log('WEB_APP_URL',WEB_APP_URL)
+const inlineKeyboard = new InlineKeyboard().webApp("Launch", {url: WEB_APP_URL});
 
 bot.command("start", async (ctx) => {
   await ctx.reply("Hello! You can open the Miktool app by clicking the button below.", { reply_markup: inlineKeyboard });
